a59c1a80c4e949570959ed3e277ff9084644f604
Österle Nicolai <oesterle.ni@onlinehome.de>
2023-09-03 07:56:50 +0200

    adding selction funktion

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let questions = [
       "answer_2": "München",
       "answer_3": "Hamburg",
       "answer_4": "Köln",
-      "result": "Berlin"
+      "result": "1"
     },
     {
       "question": "Wie viele Tage hat ein Jahr?",
@@ -13,7 +13,7 @@ let questions = [
       "answer_2": "366",
       "answer_3": "364",
       "answer_4": "360",
-      "result": "365"
+      "result": "1"
     },
     {
       "question": "Was ist die größte planetarische Anhäufung von Sand in der Welt?",
@@ -21,7 +21,7 @@ let questions = [
       "answer_2": "Gobi-Wüste",
       "answer_3": "Atacama-Wüste",
       "answer_4": "Kalahari-Wüste",
-      "result": "Sahara-Wüste"
+      "result": "1"
     },
     {
       "question": "Wer schrieb 'Romeo und Julia'?",
@@ -29,7 +29,7 @@ let questions = [
       "answer_2": "William Shakespeare",
       "answer_3": "Mark Twain",
       "answer_4": "Jane Austen",
-      "result": "William Shakespeare"
+      "result": "2"
     },
     {
       "question": "Welches Gas atmen Pflanzen ein und welches geben sie aus?",
@@ -37,7 +37,7 @@ let questions = [
       "answer_2": "Kohlendioxid ein, Sauerstoff aus",
       "answer_3": "Stickstoff ein, Sauerstoff aus",
       "answer_4": "Wasserstoff ein, Sauerstoff aus",
-      "result": "Kohlendioxid ein, Sauerstoff aus"
+      "result": "2"
     },
     {
       "question": "Was ist die Hauptkomponente der Erdatmosphäre?",
@@ -45,7 +45,7 @@ let questions = [
       "answer_2": "Sauerstoff",
       "answer_3": "Kohlendioxid",
       "answer_4": "Wasserstoff",
-      "result": "Stickstoff"
+      "result": "1"
     },
     {
       "question": "Was ist die größte Insel der Welt?",
@@ -53,7 +53,7 @@ let questions = [
       "answer_2": "Madagaskar",
       "answer_3": "Australien",
       "answer_4": "Grönland",
-      "result": "Grönland"
+      "result": "4"
     },
     {
       "question": "Wie viele Millimeter sind in einem Zentimeter?",
@@ -61,7 +61,7 @@ let questions = [
       "answer_2": "100",
       "answer_3": "1.000",
       "answer_4": "100.000",
-      "result": "10"
+      "result": "1"
     },
     {
       "question": "Welches Element hat die chemische Bezeichnung 'Fe'?",
@@ -69,7 +69,7 @@ let questions = [
       "answer_2": "Eisen",
       "answer_3": "Kalium",
       "answer_4": "Natrium",
-      "result": "Eisen"
+      "result": "2"
     },
     {
       "question": "Wie viele Kontinente gibt es auf der Erde?",
@@ -77,7 +77,7 @@ let questions = [
       "answer_2": "6",
       "answer_3": "7",
       "answer_4": "8",
-      "result": "7"
+      "result": "3"
     }
   ];
 
@@ -97,4 +97,18 @@ let currentQuestion = 0;
     document.getElementById('answer_2').innerHTML = question["answer_2"];
     document.getElementById('answer_3').innerHTML = question["answer_3"];
     document.getElementById('answer_4').innerHTML = question["answer_4"];
+  }
+
+  function answer(selection){
+    let question = questions[currentQuestion];
+    console.log('Selectet answer is ', selection);
+    let selectedQuestionNumber = selection.slice(-1);
+    console.log('selectedQuestionNumber is ', selectedQuestionNumber);
+    console.log('current question is', question['result']);
+
+    if(selectedQuestionNumber == question['result']){
+        console.log('Deine Antwort ist Richtig!');
+    }else{
+        console.log('Deine Antwort ist Falsch!!');
+    };
   }
